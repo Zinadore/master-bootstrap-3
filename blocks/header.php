0f7662d310c8d073d9cbac91a61dc72531f40a5d
harry bournis <harrybournis@gmail.com>
2016-11-23 18:18:47 +0200

    added navbar bookbook logo

--- blocks/header.php
+++ blocks/header.php
@@ -4,6 +4,7 @@
   <button type="button" class="btn btn-default" id="menu-toggle">
     <i class="fa fa-bars"></i>
   </button>
+  <img src="templates/bookbookmobile/images/mobile_version/bookbook-mobile_logo.png" class="img-responsive" id="navbar-logo">
   <span></span>
   <div class="right-menu">
     <a href="#"><i class="fa fa-key"></i></a>
